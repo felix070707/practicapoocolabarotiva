b2c81ce17c68b9b907023cddcee47bfe993a28b8
Gabriel <tijarojimenezgabriel@gmail.com>
2026-07-31 22:38:58 -0500

    Subiendo mi carpeta de alquiler de autos

---
+++ Sistema_alquiler_autos/Alquiler_autoss/src/alquiler_autoss/Alquiler_autoss.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alquiler_autoss;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Alquiler_autoss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        holaa
+    }
+    
+}
+

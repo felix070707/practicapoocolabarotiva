e058ed44c4d7e08930885d38a9b5eb1ea2359762
Manuel282002 <manuel_caviedesco@fet.edu.co>
2026-07-31 21:21:55 -0500

    Agregar proyecto Sistema de Gimnasio

---
+++ Sistema de Gimnasio/SistemaGym/src/main/java/com/mycompany/sistemagym/SistemaGym.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagym;
+
+/**
+ *
+ * @author MANUEL FELIPE
+ */
+public class SistemaGym {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

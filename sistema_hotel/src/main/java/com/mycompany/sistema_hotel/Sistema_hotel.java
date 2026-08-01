e804bfdf39f322da3a9fe74240db52fd06b59229
Johan117z <johan117herli@gmail.com>
2026-07-31 21:49:33 -0500

    Se agrego un archivo

---
+++ sistema_hotel/src/main/java/com/mycompany/sistema_hotel/Sistema_hotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_hotel;
+
+/**
+ *
+ * @author ACER
+ */
+public class Sistema_hotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

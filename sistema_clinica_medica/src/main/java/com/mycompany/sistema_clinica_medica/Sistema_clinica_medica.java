53d4bc3b45608e3b5cd2f9fef5610241a98e8b6a
Truj453 <ftg152007@gmail.com>
2026-07-31 21:16:00 -0500

    Creacion de sistema clinico

---
+++ sistema_clinica_medica/src/main/java/com/mycompany/sistema_clinica_medica/Sistema_clinica_medica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_clinica_medica;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Sistema_clinica_medica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

18f9860188bf704fe0858854572e63923ef8148c
sebcal2006 <sebastiancalderonbrinez2006@gmail.com>
2026-07-31 21:21:06 -0500

    Grupo 3

---
+++ Tienda_Virtual_Grupo _3/TiendaVirtual/src/main/java/com/mycompany/tiendavirtual/TiendaVirtual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendavirtual;
+
+/**
+ *
+ * @author sebas
+ */
+public class TiendaVirtual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
